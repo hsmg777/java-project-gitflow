98ce0d3ad3dbc742e37e384da4f886f61ac3d7ce
hsmg777 <hayland.montalvo@udla.edu.ec>
2025-06-12 22:53:44 -0500

    Preparar versión 1.1.0 con logotipo ASCII

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
+
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
 
