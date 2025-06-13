e89c8abe6deacfc4f383aa9032ca01099a6d8e56
hsmg777 <hayland.montalvo@udla.edu.ec>
2025-06-12 22:52:11 -0500

    Agregar despedida personalizada

--- Main.java
+++ Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
-        System.out.println("Versión 1.0.0 lista para producción.");
+
+        Despedida despedida = new Despedida();
+        System.out.println(despedida.generarDespedida("Mundo"));
     }
 }
